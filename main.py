07671ed48c47e887858f591a709d10af344b5bbc
Vasily Volovich <vasily.volovich@gmail.com>
2023-05-07 20:10:55 +0300

    added Globals.py file

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import numpy as np
 from tensorflow import keras
 import datetime
 
+import Globals
+
 TOKEN = "6292004438:AAEYL6PsHiX2rRYmPls5IWptP3_EWRGaO0Q"
 BOT_USERNAME = "@Cifar10Bot"
 
@@ -19,8 +21,7 @@ class AI:
         self.x_train = self.x_train / 255
         self.x_test = self.x_test / 255
 
-        self.class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog',
-                            'Frog', 'Horse', 'Ship', 'Truck']
+        self.class_names = Globals.class_names
 
         self.model = keras.models.Sequential()
         self.model.add(keras.layers.Conv2D(32, (3, 3), activation='relu',
@@ -49,9 +50,7 @@ my_AI = AI()
 
 async def handle_photo(update, context):
     if my_AI.sum_epochs_count == 0:
-        await update.message.reply_text(
-            "Bot is not trained!"
-        )
+        await update.message.reply_text(Globals.not_trained_text)
         return
     file = await context.bot.get_file(update.message.photo[-1].file_id)
     f = BytesIO(await file.download_as_bytearray())
@@ -62,42 +61,23 @@ async def handle_photo(update, context):
 
     prediction = my_AI.model.predict(np.array([img / 255]))
     await update.message.reply_text(
-        f"In this image I see a {my_AI.class_names[np.argmax(prediction)]}"
+        Globals.I_see_text({my_AI.class_names[np.argmax(prediction)]})
     )
 
 
 async def reset_command(update, context):
     global my_AI
     my_AI = AI()
-    await update.message.reply_text(
-        "Learning progress removed."
-    )
+    await update.message.reply_text(Globals.reset_command_text)
 
 
 async def start_command(update, context):
-    await update.message.reply_text(
-        "Hello! I'm Cifar10Bot! Use /help command to see, what I can do."
-    )
+    await update.message.reply_text(Globals.start_command_text)
 
 
 async def help_command(update, context):
     await update.message.reply_text(
-        "<b>I'm Cifar10Bot.</b>\n"
-        "You can type messages and I will respond to them.\n"
-        "For example you can ask me current time by typing 'time'.\n\n"
-        
-        "<b>I can classify images.</b>\n"
-        "You can send me an image with\n"
-        "Plane, Car, Bird, Cat, Deer, Dog, Frog, Horse, Ship or Truck on it.\n"
-        "And I will say, what I see on the image.\n"
-        "I'm not too good at guessing,\n"
-        "because I work with low-quality images.\n\n"
-        
-        "<b>How to train me:</b>\n"
-        "If you want to make me better at classifying, type 'train X',\n"
-        "where X is a number of epochs you want me to train.\n"
-        "You can use /reset command to remove learning progress.\n",
-
+        Globals.help_command_text,
         parse_mode=ParseMode.HTML
     )
 
@@ -109,35 +89,31 @@ async def generate_response_for_message(text, update, context):
             epochs_count = int(epochs_count)
         except ValueError:
             await update.message.reply_text(
-                "Incorrect train command!\n"
-                "Use /help command for more information."
+                Globals.incorrect_train_command_text
             )
         else:
             await update.message.reply_text(
-                "Please wait a bit, training will take "
-                f"about {epochs_count * 10} seconds"
+                Globals.wait_text(epochs_count * 10)
             )
             await my_AI.train(epochs_count)
-            await update.message.reply_text("Done! You can now send a photo!")
+            await update.message.reply_text(Globals.can_send_photo_text)
             return
 
     processed = text.lower()
-    a = ["hello", "hi", "hey"]
+    a = Globals.welcome_user_words
 
     for elem in a:
         if elem in processed:
-            await update.message.reply_text("Hey there!")
+            await update.message.reply_text(Globals.greeting)
             return
 
     if "time" in text:
         now = datetime.datetime.now()
         current_time = now.strftime("%H:%M:%S")
-        await update.message.reply_text("Current time is " + current_time)
+        await update.message.reply_text(Globals.current_time_text(current_time))
         return
 
-    await update.message.reply_text(
-        "I don't understand. Use /help command to see what I can do."
-    )
+    await update.message.reply_text(Globals.dont_understand_text)
 
 
 async def handle_message(update, context):
